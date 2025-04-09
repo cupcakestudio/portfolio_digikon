b72e7fda6ec77f9a82c7c9f5b72b5d952034a0a2
Ting Hua Lin <61328447+cupcakestudio@users.noreply.github.com>
2025-04-09 17:26:23 +0200

    content for cases i portfolio side

--- src/pages/PortfolioSide.jsx
+++ src/pages/PortfolioSide.jsx
@@ -8,18 +8,24 @@ import React, { useState } from 'react';
 
 const cases = [
   {
-    title: "Case 1",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    title: "Case 1: 'Lyt til magien!' Tivoli projekt",
+    description: "Til dette tivoli projekt, var målet: At designe en digital koncept der skabte en værdiskabende oplevelse. \nVores koncept: Lyt til magien. Et koncept udviklede til gæster i parken, med synshadicap. Konceptet består af en lydbaseret app og et taktilt kort. Appen, baseret på information om parkens forskellige zoner og miljøer, bliver beskrevet af en guide, indtalt af AI-stemmer fra EllevenLabs.Der blev arbejdet meget iterativt med design thinking og double diamond modellerne. Vi fik mulighed for at udstille vores koncept. ",
+    extradescription: "Jeg har med en gruppe, arbejdede på at besvare dette problem: 'Hvordan kan vi skabe et innovativt digitalt koncept, der gør Tivoli mere tilgængelig og fremmer inklusion for synshandicappede gæster, så de får den fulde oplevelse af Tivolis magiske have?' udviklede dette koncept om en lydbaseret guide i Tivoli parken, for at sikre gæster med synshadicap, kan få en ligeså magisk oplevelse af parken på lige med med seende gæster. Appen, baseret på information om parkens forskellige zoner og miljøer, bliver beskrevet af en guide, indtalt af AI-stemmer fra EllevenLabs. ",
+    processdescription: "Der blev arbejdet meget med grundlæggende kvalitative interviews får at få indsigter i synshadicappets hverdag.",
     image: "src/Components/images/image1_placeholder.jpg",
   },
   {
-    title: "Case 2",
-    description: "More text about another project, still just placeholder stuff.",
+    title: "Case 2: 'Vi griber dig, før ludomanien gør' - danske spil/NoA Ignite og ansvarlighed",
+    description: "En udfordring i at komme 'ansvarlighed' til livs hos Danmarks spiludbydere. Mig og min gruppes svar: Ansvarligheden skal faktisk opleves hos brugerne. Vi fik udviklet konceptet, der skal være forebyggende og støttende, således at unge mænd fastholder sunde spillevaner. Konceptet udvider Oddset-appen, da flest fra målgruppen: unge mænd under 25 år, anvender denne app mest. Der er udvidet på deres oprettelses flow, forbruget af spil bliver mere synliggjort og hjælpen bliver proaktiv fra Danske spils side af. Jeg har i dette projekt indgået i en tværfaglig team-sammensætning, hvor jeg fik mine kompetencer, indenfor projektfacilitering og idegenering, forstærket.",
+    extradescription: "Dette er et koncept der skal gribe unge mænd, der har tidligere spilleerfaring, således at de ikke forværrer deres spillevaner yderligere. Vi har hertil udviklet et særskilt oprettelsesflow for unge mænd under 25 år, hvor de skal indgive nogle økonomiske forhold (såsom indkomst) og en 'Pårørende' sektion, således en fra deres netværk (forældre eller ven) er opmærksomme på brugerens spillevaner. Når brugeren har logget ind, vil de have en forbrugsgrænse at spille for om måneden, baseret på deres angivet indkomstbeløb. For at motivere brugeren til at spille for under sit forbrug, introduceres et point system, hvor fordele kan indløses og opleves hos Danske Spils samarbejdspartnere. Sker det at brugeren ikke overholder sit anbefalet forbrug, vil danske spil tage kontakt til pågældende bruger, samtidig ville der være hjælp at hente direkte fra appen, og den angivet pårørende får også direkte besked herom. Jeg har i dette projekt indgået i en tværfaglig team-sammensætning, hvor jeg fik mine kompetencer, indenfor projektfacilitering og idegenering meget i spil, samt forstærket mine brugerundersøgelsesevner",
+    processdescription:"Vi har arbejdet med modellen Double Diamond, hvor vi i discover fasen har arbejdet divergerende i forhold til vores desk research. Vi har arbejdet på at definere det reelle problem i ‘Problem’-diamanten, som blev præsenteret i design briefen af Noa Ignite (NoAIgnite brief, 2025), (https://www.designpsykologi.dk/hvad-er-double-diamond). Parallelt med Double Diamond-modellen arbejder vi også med Google Venture ‘SPRINT’, som er designet til at kunne bygge en prototype på blot fem dage. Brug af dybdegående desk research, interviews med brugere og eksperter, samt et omfattende spørgeskema gjorde vi fandt frem til nogle indsigter, som ikke mange opdagede. 'Nemlig at Danske spil har en gruppe af unge spillere med masser af spilerfaring og aktivt bruger penge i disse spil.' Der blev arbejdet intenst under SPRINT ugen, hvor jeg også agerede Projektfacilitator for 'Sketch' dagen, altså dagen der skulle udformes på idéer og mulige løsninger, vha. Crazy 8's og kreativ tænkning.",
     image: "src/Components/images/image2_placeholder.jpg",
   },
   {
-    title: "Case 3",
-    description: "Even more project description here. Third case in carousel.",
+    title: "Case 3: Den gode morgen oplevelse",
+    description: "Et hurtig øvelse i hvordan man laver kvalitative brugerundersøgelser. Her gennemgik vi hvordan et interview bliver udført, og hvad der gør at man bliver en god interviewer.",
+    extradescription:"Hertil skulle jeg i fællesskab med mine medstuderende i et mindre team, udforme og storyboarde hvordan en future user journey for en god morgen oplevelse, vil se ud. Vi kom frem til at man kunne udvikle en skærn at opsætte i vinduet, og derved vågne op til ens ønsket morgenladskab, ved at indstille på temperaturen, lysfølsomheden, og audio. Med inspiration fra Philips hue.",
+    processdescription: "Vi har i en lille team af 2-3 mennesker udført et interview. Som forarbejde har jeg udviklet en interviewguide, og derefter fik jeg selv lov til at blive interviewet. Efter interviewet, skulle vi i teamet, finde frem til hvilke pain-points der kunne addreseres og opportunity spaces man kunne optimere på, i et service blue-print map. For derefter at udvikle og storyboarde en future user journey med konceptet indarbejdet.",
     image: "src/Components/images/image3_placeholder.jpg",
   },
 ];
@@ -115,14 +121,17 @@ function PortfolioSide() {
             exit={{ height: 0, opacity: 0 }}
             transition={{ duration: 0.4 }}
           >
+
+            <h3>{currentCase.title}</h3>
             <p>
-              This is extra content for {currentCase.title}. You can customize this per project.
+               {currentCase.extradescription}
             </p>
-          </motion.div>
+            </motion.div>
         )}
       </AnimatePresence>
+
       {/* visible container only if btn read more is cliked */}
-      {visMere && (
+      {visMere && currentCase.processdescription && (
         <motion.div
         className="readMore_container"
         initial={{ height: 0, opacity: 0 }}
@@ -131,9 +140,10 @@ function PortfolioSide() {
         transition={{ duration: 0.4 }}
         
       >
+        <h3>Proces:</h3>
         <p>
-          Why do we use it? It is a long established fact that a reader will be distracted...
-        </p>
+         {currentCase.processdescription}
+        </p><img></img>
       </motion.div>
       )}
 
