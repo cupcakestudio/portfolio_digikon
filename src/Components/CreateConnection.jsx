9a7c8567d9621f90ac74bc916d9d3bba51b7845c
Ting Hua Lin <61328447+cupcakestudio@users.noreply.github.com>
2025-05-18 21:42:13 +0200

    chabge chair pathname

--- src/Components/CreateConnection.jsx
+++ src/Components/CreateConnection.jsx
@@ -15,7 +15,7 @@ function CreateConnection() {
           </div>
         </div>
         <div className="contact-image">
-          <img src="src/Components/images/chair3.png" alt="Chair" />
+          <img src="/images/chair3.png" alt="Chair" />
         </div>
       </div>
     </section>
